a83477e7050084cc29a31aa9f94a445e27e24028
MisterJerry <misterjerry@sju.ac.kr>
2023-11-13 20:28:45 +0900

    대부분 구현

--- app/src/main/java/com/example/alomtest/Home.kt
+++ app/src/main/java/com/example/alomtest/Home.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextClock
 import com.example.alomtest.databinding.FragmentHomeBinding
@@ -86,6 +87,9 @@ class Home : Fragment() {
 //            override fun onStartTrackingTouch(p0: SeekBar?) {}
 //            override fun onStopTrackingTouch(p0: SeekBar?) {}
 //        })
+        //edittext를 임의수정 못하게 막은 코드
+        val saying=binding.editTextText
+        saying.keyListener = null
     }
 
 
