0a84831b283d752cc2086600bb0094b06df86205
KunHee Jung <misterjerry@sju.ac.kr>
2023-11-13 17:25:41 +0900

    tmp(12일 작업물)

--- app/src/main/java/com/example/alomtest/Home.kt
+++ app/src/main/java/com/example/alomtest/Home.kt
@@ -33,59 +33,59 @@ class Home : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mSeekBar = binding.seekBar
-        //이미지 겹칠 때 앞서게 설정
-        val image1=binding.imageView4
-        val image2=binding.imageView5
-        image2.bringToFront();
-
-        mSeekBar.isEnabled = false //
-        // Set the min, max and current
-        // values to the SeekBar
-        //tes
-        var mMin = 0
-        var mMax = 30
-        var mCurrent = 20
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mSeekBar.min = mMin
-            mSeekBar.max = mMax
-        }
-        //bmi계산 후 seekbar의 thumb위치 설정
-        var weight:Double
-        var height:Double
-
-
-        weight=80.0
-        height=1.74
-
-        var bmi:Double
-
-
-
-        bmi=weight/(height*height)
-
-
-        println(bmi)
-
-        mSeekBar.thumbOffset= (bmi).toInt()
-
-
-
-
-        // Set the current to progress
-        // and display in the TextView
-        mSeekBar.progress = mCurrent
+//        val mSeekBar = binding.seekBar
+//        //이미지 겹칠 때 앞서게 설정
+//        //val image1=binding.imageView4
+//        //val image2=binding.imageView5
+//        //image2.bringToFront();
+//
+//        mSeekBar.isEnabled = false //
+//        // Set the min, max and current
+//        // values to the SeekBar
+//        //tes
+//        var mMin = 0
+//        var mMax = 30
+//        var mCurrent = 20
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            mSeekBar.min = mMin
+//            mSeekBar.max = mMax
+//        }
+//        //bmi계산 후 seekbar의 thumb위치 설정
+//        var weight:Double
+//        var height:Double
+//
+//
+//        weight=80.0
+//        height=1.74
+//
+//        var bmi:Double
+//
+//
+//
+//        bmi=weight/(height*height)
+//
+//
+//        println(bmi)
+//
+//        mSeekBar.thumbOffset= (bmi).toInt()
+//
+//
+//
+//
+//        // Set the current to progress
+//        // and display in the TextView
+//        mSeekBar.progress = mCurrent
 
         // On Change Listener to change
         // current values as drag
-        mSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                mCurrent = p1
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
-        })
+//        mSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+//                mCurrent = p1
+//            }
+//
+//            override fun onStartTrackingTouch(p0: SeekBar?) {}
+//            override fun onStopTrackingTouch(p0: SeekBar?) {}
+//        })
     }
 
 
