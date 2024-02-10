b0f578a89197055f477ca473163838544220140a
MisterJerry <misterjerry@sju.ac.kr>
2024-02-10 23:29:28 +0900

    _해줘!!_

--- app/src/main/java/com/example/alomtest/login/create_account_success.kt
+++ app/src/main/java/com/example/alomtest/login/create_account_success.kt
@@ -1,7 +1,12 @@
 package com.example.alomtest.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.alomtest.R
 import com.example.alomtest.databinding.ForgetPasswordLayoutBinding
 import com.example.alomtest.databinding.LoginCreateAccountSuccessBinding
 
@@ -11,9 +16,30 @@ class create_account_success:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = LoginCreateAccountSuccessBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+
+//        binding.background.setOnClickListener {
+//
+//            Log.d("root", "디버깅")
+//            val intent = Intent(this@create_account_success, first::class.java)
+//            startActivity(intent)
+//            finish()
+//
+//
+//        }
+
+        val topLayout = findViewById<ConstraintLayout>(R.id.background)
+        topLayout.setOnClickListener {
+            Log.d("root", "디버깅")
+            val intent = Intent(this@create_account_success, first::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
-
-
 
 
 }
